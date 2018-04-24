d626bc1c7dcc35389a60a3e056b3710e982c682d
hechenqingyuan <hechenqinguyan@gmail.com>
2018-04-24 14:05:06 +0800

    测试程序

--- src/Git.Print.Test/Git.Print.Test/Program.cs
+++ src/Git.Print.Test/Git.Print.Test/Program.cs
@@ -8,7 +8,7 @@ namespace Git.Print.Test
     static class Program
     {
         /// <summary>
-        /// 应用程序的主入口点。
+        /// 应用程序的主入口点。  
         /// </summary>
         [STAThread]
         static void Main()
